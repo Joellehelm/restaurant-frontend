aa09554f88a4ff5d919d7a3c0b7c9653e40327e3
Joellehelm <joellenhelm@gmail.com>
2019-12-03 14:48:58 -0600

    fetching working

--- src/components/restaurant/RestaurantCard.js
+++ src/components/restaurant/RestaurantCard.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class RestaurantCard extends Component {
+    constructor(){
+        super()
+
+    }
+
     render() {
         return (
             <div>
-                
+                <h1>HELLo</h1>
+                <img src={this.props.photo} />
             </div>
         );
     }
--- src/components/restaurant/Restaurants.js
+++ src/components/restaurant/Restaurants.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RestaurantCard from './RestaurantCard'
 
 class Restaurants extends Component {
     constructor(){
@@ -6,7 +7,8 @@ class Restaurants extends Component {
 
         this.state = {
             location: null,
-            foodType: ""
+            foodType: "",
+            data: null
         }
     }
 
@@ -15,30 +17,46 @@ class Restaurants extends Component {
     componentDidMount(){
         const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
+        this.setState({url: url})
 
         fetch(proxyurl + url)
         .then(r => r.json())
         .then(response => {
-            response.results.map(place => {
-                (place.photos.map(pic => {
-                    console.log(pic.html_attributions[0])
-                    
+            this.setState({data: response.results})
+        })
+    
+    }
+
+  
+
+        
+    showRestaurants = () => {
+        
+        return this.state.data.map((place, idx) => {
+            const picture = (place.photos.map(pic => {
+                const imageurl = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + pic.photo_reference + '&key=' + process.env.REACT_APP_GOOGLE_API_KEY; 
+                return imageurl
                 }))
-                // place.name
-                // place.openinghours
-                // place.rating
-                // place.pricelevel
-                
-            })
+            
+         
+           return <RestaurantCard name={place.name} photo={picture} rating={place.rating} pricelevel={place.pricelevel} key={idx}/>
         })
     }
 
 
+
+
     render() {
+
+        if (!this.state.data){
+            return <div />
+        }
+
         return (
             <div>
-                <h1>HELLO from restaurants</h1>
+               {this.showRestaurants()}
+              
+                
             </div>
         );
     }
