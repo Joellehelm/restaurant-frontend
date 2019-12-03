797a42bae604ecd71ca4ac055d0097da04f195fe
Joellehelm <joellenhelm@gmail.com>
2019-12-03 10:52:46 -0600

    component changes

--- src/components/restaurant/RestaurantContainer.js
+++ src/components/restaurant/RestaurantContainer.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Restaurants from './Restaurants'
-import Reviews from './Reviews'
+import Reviews from './reviews'
 
 class RestaurantContainer extends Component {
+
+
+    
     render() {
         return (
             <div>
                 <Restaurants user={this.props.user} />
-                <Reviews user={this.props.user}/>
+                
             </div>
         );
     }
---
+++ src/components/restaurant/ShowRestaurant.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowRestaurant extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ShowRestaurant;
