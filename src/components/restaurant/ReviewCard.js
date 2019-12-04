56fa253e6b75fb83d4064023cf9eca49609468c0
Joellehelm <joellenhelm@gmail.com>
2019-12-04 15:59:59 -0600

    comment cards show now

--- src/components/restaurant/ReviewCard.js
+++ src/components/restaurant/ReviewCard.js
@@ -4,7 +4,8 @@ class ReviewCard extends Component {
     render() {
         return (
             <div>
-                
+             <h4>{this.props.comment}</h4>
+            <h3>user that made the comment</h3>
             </div>
         );
     }
