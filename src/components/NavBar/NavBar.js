0ce9b2c96746265a4f02693df5224838a137c52f
Joellehelm <joellenhelm@gmail.com>
2019-12-05 13:19:36 -0600

    style change

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,13 +3,13 @@ import './NavBar.css'
 
 
 class NavBar extends Component {
+    
+   
+
+
     render() {
         return (
-            <div>
-                {/* <div class="menu-toggle" id="hamburger">
-            <i class="fas fa-bars"></i>
-        </div> */}
-        {/* <div class="overlay"></div> */}
+            
         <div class="container">
             <nav>
                 <h1 class="brand"><a href="http://localhost:3000/">Br<span>a</span>nd</a></h1>
@@ -23,7 +23,8 @@ class NavBar extends Component {
         </div>
                 
                 
-            </div>
+         
+           
         );
     }
 }
